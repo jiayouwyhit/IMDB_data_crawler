aeb1c773c143c803388bb218ae8e62a33e126f65
Yong Wang <jiayouwyhit@gmail.com>
2016-07-09 15:24:48 -0400

    add a script for run all

---
+++ runAll.sh
@@ -0,0 +1,5 @@
+nohup python crawl_imdb_data.py 1 > log1.txt &
+nohup python crawl_imdb_data.py 2 > log2.txt &
+nohup python crawl_imdb_data.py 3 > log3.txt &
+nohup python crawl_imdb_data.py 4 > log4.txt &
+nohup python crawl_imdb_data.py 5 > log5.txt &
